a73b2b9719bc19caf531a6fa794a5f37cc443be3
shinilseop <shinilseop12@gmail.com>
2022-03-01 21:01:51 +0900

    backjoon
    PriorityQueue_ex04

--- CodingTest/src/backjoon/_14_PriorityQueue/Ex04.java
+++ CodingTest/src/backjoon/_14_PriorityQueue/Ex04.java
@@ -3,56 +3,133 @@ package backjoon._14_PriorityQueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Ex04 {
-    static int[] heap;
-    static int len;
+    static int[] maxHeap, minHeap;
+    static int maxLen, minLen;
 
-    public static int insertOdd(int num) {
-        heap[len++] = num;
-
-        int idx = len-1;
-        while (idx > 0 && heap[idx] > heap[(idx - 1) / 2]) {
-            int tmp = heap[idx];
-            heap[idx] = heap[(idx - 1) / 2];
-            heap[(idx - 1) / 2] = tmp;
-
-            idx = (idx - 1) / 2;
-        }
-
-        return heap[0];
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
-    public static int insertEven(int num) {
-        heap[len++] = num;
+    /**
+     * Maximum Heap
+     */
+    public static void insertMax(int num) {
+        maxHeap[maxLen++] = num;
 
-        int idx = len-1;
-        while (idx > 0 && heap[idx] < heap[(idx - 1) / 2]) {
-            int tmp = heap[idx];
-            heap[idx] = heap[(idx - 1) / 2];
-            heap[(idx - 1) / 2] = tmp;
+        int idx = maxLen - 1;
+        while ((idx - 1) / 2 >= 0 && maxHeap[(idx - 1) / 2] < maxHeap[idx]) {
+            swap(maxHeap, (idx - 1) / 2, idx);
+            idx = (idx-1)/2;
+        }
+    }
 
-            idx = (idx - 1) / 2;
+    public static int removeMax() {
+        int maxV = maxHeap[0];
+
+        maxHeap[0] = maxHeap[--maxLen];
+        int idx = 0;
+        while (idx * 2 + 1 < maxLen) {
+            int childIdx = idx*2+1;
+            if (childIdx+1 < maxLen && maxHeap[childIdx] < maxHeap[childIdx+1]) {
+                childIdx++;
+            }
+
+            if (maxHeap[idx] < maxHeap[childIdx]) {
+                swap(maxHeap, idx, childIdx);
+                idx = childIdx;
+            } else {
+                break;
+            }
         }
 
-        return heap[0];
+        return maxV;
+    }
+
+
+    /**
+     * Minimum Heap
+     */
+    public static void insertMin(int num) {
+        minHeap[minLen++] = num;
+
+        int idx = minLen - 1;
+        while ((idx - 1) / 2 >= 0 && minHeap[(idx - 1) / 2] > minHeap[idx]) {
+            swap(minHeap, (idx - 1) / 2, idx);
+            idx = (idx-1)/2;
+        }
+    }
+
+    public static int removeMin() {
+        int minV = minHeap[0];
+
+        minHeap[0] = minHeap[--minLen];
+        int idx = 0;
+        while (idx * 2 + 1 < minLen) {
+            int childIdx = idx*2+1;
+            if (childIdx+1 < minLen && minHeap[childIdx] > minHeap[childIdx+1]) {
+                childIdx++;
+            }
+
+            if (minHeap[idx] > minHeap[childIdx]) {
+                swap(minHeap, idx, childIdx);
+                idx = childIdx;
+            } else {
+                break;
+            }
+        }
+
+        return minV;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
-        heap = new int[size];
-        len = 0;
+        maxHeap = new int[size];
+        minHeap = new int[size];
+        maxLen = minLen = 0;
 
+
+        int mid = Integer.parseInt(br.readLine());
         StringBuffer sb = new StringBuffer(size * 2);
-        for (int i = 0; i < size; i++) {
+        sb.append(mid).append('\n');
+        int len = 1;
+        for (int i = 1; i < size; i++) {
             int num = Integer.parseInt(br.readLine());
+            len++;
 
-            if ((len+1) % 2 == 0) {
-                sb.append(insertEven(num)).append('\n');
-            } else {
-                sb.append(insertOdd(num)).append('\n');
+            if (len % 2 == 0) { // 짝수이므로 minHeap에 큰 값을 저장
+                if (mid < num) {
+                    insertMin(num);
+                } else {
+                    insertMin(mid);
+//                    System.out.println("min : "+minLen+" && "+minHeap[0]+" > "+num);
+                    if (maxLen > 0 && maxHeap[0] > num) {
+                        mid = removeMax();
+                        insertMax(num);
+                    } else {
+                        mid = num;
+                    }
+                }
+            } else { // 홀수이므로 maxHeap에 작은 값을 저장
+                if (mid > num) {
+                    insertMax(num);
+                } else {
+                    insertMax(mid);
+                    if (minLen > 0 && minHeap[0] < num) {
+                        mid = removeMin();
+                        insertMin(num);
+                    } else {
+                        mid = num;
+                    }
+                }
             }
+
+            sb.append(mid).append('\n');
         }
 
         System.out.println(sb.toString());
