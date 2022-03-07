264933fd8bb7ae6c725372473532c1fb8dbb81ad
shinilseop <shinilseop12@gmail.com>
2022-03-08 05:44:45 +0900

    programmers
    tmp

--- CodingTest/src/programmers/high_scores/_07_dynamic_programming/Ex04.java
+++ CodingTest/src/programmers/high_scores/_07_dynamic_programming/Ex04.java
@@ -1,6 +1,7 @@
 package programmers.high_scores._07_dynamic_programming;
 
 public class Ex04 {
+    
     public int solution(int[] money) {
         int[][] dp = new int[money.length][2];
         dp[0][0] = money[0];
@@ -24,6 +25,30 @@ public class Ex04 {
         }
 
         int max2 = Math.max(dp[dp.length-2][0],Math.max(dp[dp.length-2][1],Math.max(dp[dp.length-1][0],dp[dp.length-1][1])));
+
+        return Math.max(max1, max2);
+    }
+
+    
+    // 속도+효율 굳
+    public int solution2(int[] money) {
+        int[] dp = new int[money.length];
+        dp[0] = money[0];
+        dp[1] = money[1];
+        dp[2] = Math.max(money[1], money[0] + money[2]);
+        for (int i = 3; i < money.length-1; i++) {
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i - 2] + money[i], dp[i - 3] + money[i]));
+        }
+        int max1 = Math.max(dp[money.length - 1], Math.max(dp[money.length - 2], dp[money.length - 3]));
+
+        dp = new int[money.length];
+        dp[0] = 0;
+        dp[1] = money[1];
+        dp[2] = Math.max(money[1], money[2]);
+        for (int i = 3; i < money.length; i++) {
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i - 2] + money[i], dp[i - 3] + money[i]));
+        }
+        int max2 = Math.max(dp[money.length - 1], Math.max(dp[money.length - 2], dp[money.length - 3]));
 
         return Math.max(max1, max2);
     }
