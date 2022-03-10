f7f9f50d5b6702849ed625d7cd0187e2ef8777cf
shinilseop <shinilseop12@gmail.com>
2022-03-10 21:26:27 +0900

    backjoon repeat
    math1_ex05

--- CodingTest/src/backjoon/_01_math1/Ex05_r.java
+++ CodingTest/src/backjoon/_01_math1/Ex05_r.java
@@ -3,11 +3,22 @@ package backjoon._01_math1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Ex05_r {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int floor = N % H == 0 ? H : N % H;
+            int roomNum = N % H == 0 ? N / H : N / H + 1;
+            System.out.printf("%d%02d\n", floor, roomNum);
+        }
     }
 }
