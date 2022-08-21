f2925d983353fc2d7fd5b73b0ece935d35156fc1
shinilseop <shinilseop12@gmail.com>
2022-08-22 01:49:51 +0900

    add swea 5215

--- CodingTest/src/Test.java
+++ CodingTest/src/Test.java
@@ -2,72 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Test {
-    static int n;
+    static int N;
+    static boolean[] visited;
+    public static void comb(int idx, int cnt) {
+        if (cnt == N / 2) {
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i]) {
+                    System.out.print(i);
+                }
+            }
+            System.out.println();
+        }
+
+        if (idx >= visited.length) {
+            return;
+        }
+
+        visited[idx] = true;
+        comb(idx + 1, cnt + 1);
+        visited[idx] = false;
+        comb(idx + 1, cnt);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-        n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            arr[0] = i;
-            count += dfs(1, arr);
-        }
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N];
 
-        System.out.println(count);
-        br.close();
-
-    }
-
-    public static int dfs(int row, int[] arr) { //다음 줄
-        int count = 0;
-        //System.out.println("row = " + row);
-
-        if (row == n) {
-            //System.out.println("row = " + row);
-            return 1;
-        } else {
-            for (int i = 0; i < n; i++) {
-                //검사할 원소 값
-                arr[row] = i;
-
-
-//                if(check(row,i, arr)){
-//                    count += dfs(row+1, arr);
-//                }
-
-                for (int j = 0; j < row; j++) {
-                    //arr 의 있던 친구들 위치
-                    if (i == arr[j]) {
-                        break;
-                    }
-                    if (Math.abs(j - row) == Math.abs(arr[j] - i)) break;
-                    if (j == row - 1) count += dfs(row + 1, arr);
-                }
-                for (int j = 0; j < row; j++) {
-                    //arr 의 있던 친구들 위치
-                    if (i == arr[j]) {
-                        break;
-                    }
-                    if (Math.abs(j - row) == Math.abs(arr[j] - i)) break;
-                    count += dfs(row + 1, arr);
-                }
-            }
-        }
-        return count;
-    }
-
-    static boolean check(int row, int i, int[] arr) {
-        for (int j = 0; j < row; j++) {
-            //arr 의 있던 친구들 위치
-            if (i == arr[j]) {
-                return false;
-            }
-            if (Math.abs(j - row) == Math.abs(arr[j] - i)) return false; //대각선
-        }
-
-        return true;
+        comb(0, 0);
     }
 }
---
+++ CodingTest/src/sw_expert/swea_5215.java
@@ -0,0 +1,71 @@
+package sw_expert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_5215 {
+    static boolean[] visited;
+
+    public void dfs() {
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs();
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void comb(int idx, int maxCnt, int cnt) {
+        if (maxCnt == cnt) {
+            for (int i = 0; i < visited.length; i++) {
+                if(visited[i]) System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        if (idx == visited.length) {
+            return;
+        }
+
+        visited[idx] = true;
+        comb(idx + 1, maxCnt, cnt+1);
+        visited[idx] = false;
+        comb(idx + 1, maxCnt, cnt);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            visited = new boolean[N];
+            for (int i = 1; i <= N; i++) {
+                System.out.println("==="+i+"===");
+                comb(0, i, 0);
+            }
+//            int maxCal = Integer.parseInt(st.nextToken());
+//
+//            List<Food> foods = new ArrayList<>();
+//            for (int i = 0; i < N; i++) {
+//                st = new StringTokenizer(br.readLine(), " ");
+//                foods.add(new Food(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+//            }
+
+        }
+    }
+
+    static class Food {
+        int taste;
+        int cal;
+
+        public Food(int taste, int cal) {
+            this.taste = taste;
+            this.cal = cal;
+        }
+    }
+}
