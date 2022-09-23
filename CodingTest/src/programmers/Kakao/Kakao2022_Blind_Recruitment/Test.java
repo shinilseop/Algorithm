39d3a358dc274497233269f0ae0348ebefe57ed3
shinilseop <shinilseop12@gmail.com>
2022-09-24 05:05:55 +0900

    programmers: 2022 kakao blind recruitment tested

---
+++ CodingTest/src/programmers/Kakao/Kakao2022_Blind_Recruitment/Test.java
@@ -0,0 +1,8 @@
+package programmers.Kakao.Kakao2022_Blind_Recruitment;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(Math.ceil(10.4563));
+    }
+}
