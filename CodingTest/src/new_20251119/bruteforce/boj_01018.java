8994ab5ac0fa920afd2c3271d3298a7e4f69f1b4
shinilseop <shinilseop12@gmail.com>
2025-11-22 17:32:32 +0900

    create kotlin project

---
+++ CodingTest/src/new_20251119/bruteforce/boj_01018.java
@@ -0,0 +1,7 @@
+package new_20251119.bruteforce;
+
+public class boj_01018 {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ kt/kt-algorithm/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "com.exam"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
